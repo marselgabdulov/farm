ccf3c02c1ab51405d8705be37080dafea811fcd8
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-30 09:59:07 +0300

    Migrate styles for layout, fonts and normalize.css from previos version

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,49 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <div className="header">
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <button
+            className="header__button"
+            onClick={() => this.scrollTo(document.getElementById('intro'))}
+            title="На главную"
+          >
+            Позняки
+          </button>
+        </div>
+        <nav className="header__links">
+          <button
+            className="header__button"
+            onClick={() => this.scrollTo(document.getElementById('about'))}
+            title="Подробнее"
+          >
+            О нас
+          </button>
+          <button
+            className="header__button"
+            onClick={() => this.scrollTo(document.getElementById('products'))}
+            title="Наша продукция"
+          >
+            Продукты
+          </button>
+          <button
+            className="header__button"
+            onClick={() => this.scrollTo(document.getElementById('media'))}
+            title="Соцсети"
+          >
+            Медиа
+          </button>
+          <button
+            className="header__button"
+            onClick={() => this.scrollTo(document.getElementById('contacts'))}
+            title="Связь с нами"
+          >
+            Контакты
+          </button>
+        </nav>
+      </div>
+    </div>
   </Layout>
 )
 
